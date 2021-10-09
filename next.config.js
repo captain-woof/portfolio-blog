ad3aca53d2d441956624c837351c8e862762c3f2
Captain Woof <captainwoof.official@gmail.com>
2021-10-09 12:07:29 +0530

    Integrated Contentful

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    domains: ['images.ctfassets.net']
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,65 @@
 import SectionOne from '../components/Containers/Portfolio/section-1'
 import SectionTwo from '../components/Containers/Portfolio/section-2'
+import SectionThree from '../components/Containers/Portfolio/section-3'
+import SectionFour from '../components/Containers/Portfolio/section-4'
 import SeoPortfolio from '../components/SEO/SeoPortfolio'
+import { getContenfulClient } from '../lib/contentful'
 
-export default function Index() {
+// Get static props at build time
+export const getStaticProps = async () => {
+  // Fetch data from Contentful
+  const contentfulClient = getContenfulClient()
+  const projectDataPromise = contentfulClient.getEntries({
+    content_type: 'project'
+  })
+  const openSourceContribDataPromise = contentfulClient.getEntries({
+    content_type: 'openSourceContributions'
+  })
+  const [projectDataContentful, openSourceContribDataContentful] = await Promise.all([projectDataPromise, openSourceContribDataPromise])
+
+  // Parse and prepare data to pass as props in containers
+  const projectData = projectDataContentful.items.map((data) => ({
+    title: data.fields.title,
+    description: data.fields.description,
+    tags: data.fields.tags,
+    liveLink: data.fields.liveLink,
+    sourceLink: data.fields.sourceLink,
+    backgroundImage: {
+      height: data.fields.backgroundImage.fields.file.details.image.height,
+      width: data.fields.backgroundImage.fields.file.details.image.width,
+      path: `https:${data.fields.backgroundImage.fields.file.url}`,
+      alt: data.fields.backgroundImage.fields.title
+    }
+  }))
+
+  const openSourceContribData = openSourceContribDataContentful.items.map((data) => ({
+    title: data.fields.title,
+    description: data.fields.description,
+    tags: data.fields.tags,
+    liveLink: data.fields.liveLink,
+    sourceLink: data.fields.sourceLink,
+    backgroundImage: {
+      height: data.fields.backgroundImage.fields.file.details.image.height,
+      width: data.fields.backgroundImage.fields.file.details.image.width,
+      path: `https:${data.fields.backgroundImage.fields.file.url}`,
+      alt: data.fields.backgroundImage.fields.title
+    }
+  }))
+
+  return ({
+    props: { projectData, openSourceContribData },
+    revalidate: 60, // 1 minute
+  })
+}
+
+export default function Index({ projectData, openSourceContribData }) {
   return (
     <>
       <SeoPortfolio />
       <SectionOne />
       <SectionTwo />
+      <SectionThree projectData={projectData} />
+      <SectionFour openSourceContribData={openSourceContribData} />
     </>
   )
 }
