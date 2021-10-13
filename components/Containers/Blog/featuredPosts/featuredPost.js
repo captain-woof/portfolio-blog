031e1679d95cea0fd1e008e7ac6959464ce4f1cb
Captain Woof <captainwoof.official@gmail.com>
2021-10-13 17:05:25 +0530

    Featured posts slideshow ready

--- components/Containers/Blog/featuredPosts/featuredPost.js
+++ components/Containers/Blog/featuredPosts/featuredPost.js
@@ -1,6 +1,10 @@
-import styled from 'styled-components'
-import { motion, useAnimation } from 'framer-motion'
+import styled, { css } from 'styled-components'
+import { motion } from 'framer-motion'
 import { easeOutQuintBezier } from '../../../../lib/motion'
+import moment from 'moment'
+import Link from 'next/link'
+import { useGlobalContext } from '../../../../providers/ContextProvider'
+import { getFirstWords, getWordCount } from '../../../../utils/wordfu'
 
 const FeaturedPostContainer = styled(motion.div)`
     height: 100%;
@@ -8,9 +12,74 @@ const FeaturedPostContainer = styled(motion.div)`
     position: absolute;
     background-color: ${({ color }) => color};
     top: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    font-family: 'Poppins';
+    color: ${({ theme }) => theme.colors.white};
+`
+
+const FeaturedPostContentPadding = styled.div`
+    position: relative;
+    height: 100%;
+    width: 100%;
+    padding: 6rem 10rem;
+
+    ${({ theme: { isPhone } }) => (isPhone && css`
+        padding: 8rem 3.2rem;
+    `)}
+`
+
+const FeaturedPostContentContainer = styled.div`
+    position: relative;
+`
+
+const FeaturedPostHeading = styled.div`
+    font-size: 1.8rem;    
+    ${({ theme: { isPhone } }) => (isPhone && css`
+        font-size: 1.4rem;
+    `)}
+`
+
+const PostTitle = styled.div`
+    font-size: 3rem;
+    font-weight: 500;
+    margin-bottom: 0.5rem;
+    ${({ theme: { isPhone } }) => (isPhone && css`
+        font-size: 2rem;
+    `)}
+
+    &:hover {
+        text-decoration: underline;
+    }
+`
+
+const PostTimestampIcon = styled.img`
+    height: 1rem;
+    margin-right: 0.5rem;
+`
+
+const PostTimestamp = styled.div`
+    font-size: 0.9rem;
+    font-style: italic;
+`
+
+const Tag = styled.div`
+    font-size: 0.8rem;
+    margin-top: 0.5rem;
+    color: ${({ color }) => color};
+    background-color: ${({ theme }) => theme.colors.white};
+    padding: 0.1rem 0.5rem;
+    border-radius: 1rem;
+    width: max-content;
+`
+
+const PostDescription = styled.div`
+    margin-top: 0.5rem;
+    font-size: 1rem;
+    width: 60%;
+    font-weight: 500;
+
+    ${({ theme: { isPhone } }) => (isPhone && css`
+        width: 100%;
+    `)}
 `
 
 const transition = {
@@ -33,9 +102,30 @@ const variants = {
 }
 
 export default function FeaturedPost({ featuredPost }) {
+    const { globalState: { isPhone } } = useGlobalContext()
+
     return (
         <FeaturedPostContainer color={featuredPost.tags[0].color} animate='animate' initial='initial' exit='exit' variants={variants}>
-            <h1>{featuredPost.title}</h1>
+            <FeaturedPostContentPadding>
+                <FeaturedPostContentContainer>
+                    <FeaturedPostHeading>Featured post</FeaturedPostHeading>
+                    <Link href={`/blog/posts/${featuredPost.slug}`}><a>
+                        <PostTitle >{featuredPost.title}</PostTitle>
+                    </a></Link>
+                    <PostTimestamp>
+                        <PostTimestampIcon src='/icons/calendar-night.svg' alt='posted on icon' />
+                        {`Posted ${moment(featuredPost.postedOn).fromNow()}`}
+                    </PostTimestamp>
+                    <PostTimestamp>
+                        <PostTimestampIcon src='/icons/update-night.svg' alt='updated on icon' />
+                        {`Last updated ${moment(featuredPost.updatedOn).fromNow()}`}
+                    </PostTimestamp>
+                    <Tag color={featuredPost.tags[0].color}>{featuredPost.tags[0].name}</Tag>
+                    <PostDescription>
+                        {isPhone ? (getWordCount(featuredPost.title) > 14 ? null : getFirstWords(featuredPost.description, 25)) : featuredPost.description}
+                    </PostDescription>
+                </FeaturedPostContentContainer>
+            </FeaturedPostContentPadding>
         </FeaturedPostContainer>
     )
 }
--- utils/wordfu.js
+++ utils/wordfu.js
@@ -1,10 +1,12 @@
 export const getFirstWords = (string, n) => {
     const spaceIndices = []
-    for(let i = 0; i < string.length; i++){
-        if(string[i] === ' '){
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === ' ') {
             spaceIndices.push(i)
         }
     }
     const lastCharIndex = spaceIndices[n - 1]
     return string.slice(0, lastCharIndex)
 }
+
+export const getWordCount = (string) => (string.split(' ').length)
