da74140b90260b625b49393ac2b00fc9553833cb
Captain Woof <captainwoof.official@gmail.com>
2021-10-15 15:55:24 +0530

    Added watcher text

--- components/Containers/PostsSummaryContainer/index.js
+++ components/Containers/PostsSummaryContainer/index.js
@@ -1,22 +1,56 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useIntersectionRevealer } from 'react-intersection-revealer'
-import { useState } from 'react'
 import { useInfinitePostSummaries } from '../../../lib/swr'
 import PostSummaryCard from './card'
 import Grid from './grid'
+import styled from 'styled-components'
+import { useThemeChangeAnim } from '../../../lib/motion'
+import { motion } from 'framer-motion'
+
+const Watcher = styled(motion.div)`
+    font-size: 1rem;
+    font-weight: 300;
+    margin-top: 1rem;
+`
 
 export default function PostsSummaryContainer({ firstPostsSummary, tag }) {
     const { postSummaries, loadOneMorePage, noMoreData } = useInfinitePostSummaries(firstPostsSummary, tag)
+    const { textSubtitlesVariants: watcherVariants, textSubtitlesAnimation: watcherAnimation } = useThemeChangeAnim({
+        textSubtitlesVariants: {
+            initial: {
+                opacity: 1
+            },
+            pulse: {
+                opacity: 0.5,
+                ease: 'linear',
+                duration: 0.75,
+                repeat: Infinity,
+                repeatType: 'mirror'
+            }
+        }
+    })
 
     // For auto fetching more posts
     const watcherRef = useRef()
     const { inView } = useIntersectionRevealer(watcherRef)
-    const [loadMorePageInterval, setLoadMorePageInterval] = useState(null)
     useEffect(() => {
         if (inView) {
+            watcherAnimation.start('pulse')
             loadOneMorePage()
+        } else {
+            watcherAnimation.stop()
         }
     }, [inView])
+
+    // For showing if there are any posts to load
+    const [watcher, setWatcher] = useState("Loading...")
+    useEffect(() => {
+        if (noMoreData) {
+            setWatcher("No more posts to show.")
+            watcherAnimation.stop()
+            watcherAnimation.set('initial')
+        }
+    }, [noMoreData])
 
     return (
         <>
@@ -25,7 +59,9 @@ export default function PostsSummaryContainer({ firstPostsSummary, tag }) {
                     <PostSummaryCard {...postSummaryData} key={index} />
                 ))}
             </Grid>
-            <div className='watcher' ref={watcherRef} />
+            <Watcher className='watcher' ref={watcherRef} animate={watcherAnimation} variants={watcherVariants} initial='initial'>
+                {watcher}
+            </Watcher>
         </>
     )
 }
