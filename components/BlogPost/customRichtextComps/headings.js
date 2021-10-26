1cd987dae8a5f6b051c42a8aacc64d5cce01cb0b
Sohail Saha <captainwoof.official@gmail.com>
2021-10-27 00:24:41 +0530

    Headings and Para (Blog post) lh & ls increased

--- components/BlogPost/customRichtextComps/headings.js
+++ components/BlogPost/customRichtextComps/headings.js
@@ -4,25 +4,25 @@ import { slugify } from "../../../utils/wordfu"
 
 export const Heading1 = ({children}) => {
     const {textEmphasisAnimation: animate, textEmphasisVariants: variants} = useThemeChangeAnim()
-    return <motion.h1 id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h1>
+    return <motion.h1 style={{lineHeight: 1.5}} id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h1>
 }
 export const Heading2 = ({children}) => {
     const {textEmphasisAnimation: animate, textEmphasisVariants: variants} = useThemeChangeAnim()
-    return <motion.h2 id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h2>
+    return <motion.h2 style={{lineHeight: 1.5}} id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h2>
 }
 export const Heading3 = ({children}) => {
     const {textEmphasisAnimation: animate, textEmphasisVariants: variants} = useThemeChangeAnim()
-    return <motion.h3 id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h3>
+    return <motion.h3 style={{lineHeight: 1.5}} id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h3>
 }
 export const Heading4 = ({children}) => {
     const {textEmphasisAnimation: animate, textEmphasisVariants: variants} = useThemeChangeAnim()
-    return <motion.h4 id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h4>
+    return <motion.h4 style={{lineHeight: 1.8}} id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h4>
 }
 export const Heading5 = ({children}) => {
     const {textEmphasisAnimation: animate, textEmphasisVariants: variants} = useThemeChangeAnim()
-    return <motion.h5 id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h5>
+    return <motion.h5 style={{lineHeight: 1.8}} id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h5>
 }
 export const Heading6 = ({children}) => {
     const {textEmphasisAnimation: animate, textEmphasisVariants: variants} = useThemeChangeAnim()
-    return <motion.h6 id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h6>
+    return <motion.h6 style={{lineHeight: 1.8}} id={slugify(children)} animate={animate} variants={variants}>{children}</motion.h6>
 }
--- components/BlogPost/customRichtextComps/paragraph.js
+++ components/BlogPost/customRichtextComps/paragraph.js
@@ -1,16 +1,26 @@
 import { motion } from 'framer-motion'
 import { useThemeChangeAnim } from '../../../lib/motion'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const ParagraphWrapper = styled(motion.p)`
-    a {
-        text-decoration: underline;
-        font-style: italic;
-    }
+    ${({ theme }) => css`
+        line-height: 2.2;
+        letter-spacing: 0.015rem;
+
+        & a {
+            text-decoration: underline;
+            font-style: italic;
+            color: ${theme.colors.blue};
+        }
+
+        & a:hover {
+            color: ${theme.colors.blueDark};
+        }
+    `}
 `
 
-export default function Paragraph({children}){
-    const {textEmphasisVariants: variants, textEmphasisAnimation: animate} = useThemeChangeAnim()
+export default function Paragraph({ children }) {
+    const { textEmphasisVariants: variants, textEmphasisAnimation: animate } = useThemeChangeAnim()
 
     return (
         <ParagraphWrapper variants={variants} animate={animate}>{children}</ParagraphWrapper>
